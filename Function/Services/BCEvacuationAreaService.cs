77f3c6e74b80ecdfb4f32a00e4b59e899708ce90
greg <greg@mardon.ca>
2024-07-24 13:41:35 -0600

    Support updated arcgis response.
    Fixing bad ID type.

--- Function/Services/BCEvacuationAreaService.cs
+++ Function/Services/BCEvacuationAreaService.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EvacAlert.Data;
 using GeoJSON.Text.Feature;
@@ -20,7 +21,7 @@ namespace EvacAlert.Services
         /// In GeoJson
         /// </summary>
         const string EvacUrlWMS = "https://openmaps.gov.bc.ca/geo/pub/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&SRS=EPSG:4326&LAYERS=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&STYLES=6885&FORMAT=application/json;type=geojson&TRANSPARENT=TRUE&maxFeatures=200&format_options=KMATTR:true;KMSCORE:25;MODE:refresh;SUPEROVERLAY:false&bbox=-179,-80,80,179&height=10000000&width=10000000";
-        const string EvacUrlArcGIS = "https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=standard&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=EMRG_OAA_SYSID%2C+EVENT_NAME%2C+EVENT_NUMBER%2C+EVENT_TYPE%2C+ORDER_ALERT_STATUS%2C+ISSUING_AGENCY%2C+PREOC_CODE%2C+DATE_MODIFIED%2C+FEATURE_AREA_SQM%2C+FEATURE_LENGTH_M%2C+MULTI_SOURCED_HOMES%2C+MULTI_SOURCED_POPULATION%2C+ORDER_ALERT_NAME%2C+OBJECTID&returnGeometry=true&returnCentroid=true&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnTrueCurves=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
+        const string EvacUrlArcGIS = "https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Evacuation_Orders_and_Alerts/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=standard&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=EMRG_OAA_SYSID%2C+EVENT_NAME%2C+EVENT_NUMBER%2C+EVENT_TYPE%2C+ORDER_ALERT_STATUS%2C+ISSUING_AGENCY%2C+PREOC_CODE%2C+DATE_MODIFIED%2C+FEATURE_AREA_SQM%2C+FEATURE_LENGTH_M%2C+MULTI_SOURCED_HOMES%2C+MULTI_SOURCED_POPULATION%2C+ORDER_ALERT_NAME%2C+OBJECTID&returnGeometry=true&returnCentroid=true&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnTrueCurves=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=geojson&token=";
 
         public BCEvacuationAreaService(HttpClient httpClient)
         {
@@ -32,7 +33,7 @@ namespace EvacAlert.Services
             string[] urlsToTry = new string[]
             {
                 EvacUrlWMS,
-                //EvacUrlArcGIS temporarily disable. 
+                EvacUrlArcGIS
             };
 
             List<Exception> exceptionsOccurred = new List<Exception>();
@@ -62,6 +63,7 @@ namespace EvacAlert.Services
 
         private List<EvacuationArea> GetCurrentEvacuationAreasFromGeoJson(string jsonContent)
         {
+            jsonContent = FixBadGeoJsonIds(jsonContent);
             List<EvacuationArea> evacAreas = new List<EvacuationArea>();
 
             FeatureCollection evacFeatureCollection = JsonSerializer.Deserialize<FeatureCollection>(jsonContent);
@@ -93,9 +95,14 @@ namespace EvacAlert.Services
                     evacArea.NumberOfHomesAffected = numHomes as int?;
                 }
                 if (evacFeature.Properties.TryGetValue("DATE_MODIFIED", out object dateModifiedString) &&
-                    DateTime.TryParseExact(dateModifiedString.ToString().Replace("Z",""), "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime modifiedDate))
+                    DateTime.TryParseExact(dateModifiedString.ToString().Replace("Z", ""), "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime modifiedDate))
                 {
                     evacArea.DateModified = modifiedDate;
+                }
+                else if (evacFeature.Properties.TryGetValue("DATE_MODIFIED", out object dateModifiedUnixTime) &&
+                        Regex.IsMatch(dateModifiedUnixTime.ToString(), "^0-9+$"))
+                {
+                    //if it's a unix timestamp, then make the change
                 }
 
 
@@ -134,6 +141,20 @@ namespace EvacAlert.Services
             }
 
             return evacAreas;
+        }
+
+
+        /// <summary>
+        /// some services are incorrectly sending id's as ints, instead of strings.  BAD.
+        /// We fix. 
+        /// </summary>
+        /// <param name="jsonContent"></param>
+        /// <returns></returns>
+        private string FixBadGeoJsonIds(string jsonContent)
+        {
+            string pattern = @"""id"":\s*?([0-9]+)";//an id followed by numbers
+            string replacement = @"""id"": ""$1"""; //soccurnd the value by quotes
+            return Regex.Replace(jsonContent, pattern, replacement);
         }
     }
 }
